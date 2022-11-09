0f830739e0e966a8969955f5d563f86b9dcdfbbf
Pushpit <pushpit07@gmail.com>
2022-11-09 23:44:53 +0530

    Trying self-hosted server on vercel
    Signed-off-by: Pushpit <pushpit07@gmail.com>

--- server.ts
+++ server.ts
@@ -31,6 +31,14 @@ app.use(
 app.use(`/server`, parseServer.app);
 app.use('/dashboard', parseDashboard);
 
+app.get('/', (req, res) => {
+  res.send(
+    '<html><head><title>Musixverse Parse Server</title></head><body style="font-family: Poppins !important; background-color: black; padding: 0; margin:0;">' +
+      '<div style="display: flex; flex:1; height: 100% ; justify-content: center; align-items: center; min-height: 100vh !important; font-size: 28px !important; color: #5AB510 !important;">' +
+      'Musixverse Parse Server is running...</div></body></html>',
+  );
+});
+
 const httpServer = http.createServer(app);
 httpServer.listen(config.PORT, () => {
   // eslint-disable-next-line no-console
