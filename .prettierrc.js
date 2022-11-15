994e0b160da7ec52cbdbdcb7890c6e64c2e8d943
Pushpit <pushpit07@gmail.com>
2022-11-15 19:53:26 +0530

    Added CI/CD for AWS ECS Containers
    Signed-off-by: Pushpit <pushpit07@gmail.com>

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,6 @@
 module.exports = {
-  printWidth: 120,
-  tabWidth: 4,
-  singleQuote: true,
-  trailingComma: 'all',
+    printWidth: 120,
+    tabWidth: 2,
+    singleQuote: true,
+    trailingComma: 'all',
 };
